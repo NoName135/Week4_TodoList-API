a16d091e0d6f49098bfcdf64edd135948142926c
Karuma Lin <qscwdvefb3@gmail.com>
2022-09-04 03:22:46 +0800

    remove React strick mode

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
